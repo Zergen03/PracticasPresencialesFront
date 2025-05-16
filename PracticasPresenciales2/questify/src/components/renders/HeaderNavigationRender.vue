2bf3f22eac3e839c71c14b180a23b6eb492a8653
Alberto Rodriguez Penalva <127990307+AlbertoRodriguez4@users.noreply.github.com>
2025-05-16 12:32:58 +0200

    changes

--- PracticasPresenciales2/questify/src/components/renders/HeaderNavigationRender.vue
+++ PracticasPresenciales2/questify/src/components/renders/HeaderNavigationRender.vue
@@ -3,17 +3,30 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 <template>
-  <div class="header-container">
-    <header class="navbar">
-      <nav class="nav-links">
-        <RouterLink to="/" class="nav-item">Home</RouterLink>
-        <RouterLink to="/store" class="nav-item">Store</RouterLink> 
-        <RouterLink to="/inventory" class="nav-it">Inventory</RouterLink> 
-      </nav>
+    <v-app-bar color="blue-lighten-3" flat>
+        <v-container class="d-flex align-center justify-space-between" fluid>
+            <!-- Sección izquierda -->
+            <div class="d-flex align-center">
+                <v-btn text class="text-none font-weight-bold text-capitalize">
+                    Questify
+                </v-btn>
 
-      </header>
-  </div>
+                <RouterLink to="/" class="mx-2" >Tareas</RouterLink>
+                <RouterLink to="/shop" class="mx-2" >Tienda</RouterLink>
+                <RouterLink to="/inventory" class="mx-2" >Inventario</RouterLink>
+            </div>
+
+            <!-- Sección derecha -->
+            <div class="d-flex align-center">
+                <v-icon class="mr-1">mdi-coin</v-icon>
+                <span class="mr-4">0</span>
+                <v-icon>mdi-account</v-icon>
+            </div>
+        </v-container>
+    </v-app-bar>
 </template>
 <style scoped>
-
+.v-app-bar {
+  height: 60px;
+}
 </style>
